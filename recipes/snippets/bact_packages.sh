4a074bdabbfb01639354aa3baba627ab1cfd18d3
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2023-12-04 17:17:16 +0100

    [TASK} simplify package installation

--- recipes/snippets/bact_packages.sh
+++ recipes/snippets/bact_packages.sh
@@ -7,16 +7,38 @@ bact-bessyii-ophyd        \
 bact2_bessyii             \
 "
 
+WHEELS_DIR_DEFAULT=file:///build/wheels
+WHEELS_DIR="$WHEELS_DIR_DEFAULT"
+
+pip_install_use_cache ()
+{
+    local status=-1
+    
+    echo "pip install: with cache $WHEELS_DIR using args " $@
+    if python3 -m pip install --find-links "$WHEELS_DIR" --no-index --check-build-dependencies $@
+    then
+	status=$?
+	echo "pip install used wheel dir $WHEELS_DIR"
+    else
+	echo "pip install used wheel dir $WHEELS_DIR returned status $?, continuing without dir" 	
+	python3 -m pip install $@
+	status=$?
+    fi
+    return $status
+}
+
+
 for i in $packages
 do
     echo "Building $i"
     git clone https://github.com/hz-b/$i.git
     cd $i
 
-    if [ -e requirements.txt ] ; then
-         pip3 install -r requirements.txt --find-links file:///build/wheels --no-index
+    if [ -f requirements.txt ] ; then
+	# if not available in the directory
+	pip_install_use_cache -r requirements.txt
     fi
-    pip3 install .
+    pip_install_use_cache . 
     cd ..
 done
 
@@ -28,11 +50,12 @@ do
     cd $i
     # git checkout dev/feature/machine-deployment
     git pull
-    if [ -e requirements.txt ] ; then
-         pip3 install -r requirements.txt --find-links file:///build/wheels --no-index
+    if [ -f requirements.txt ] ; then
+	# if not available in the directory
+	pip_install_use_cache -r requirements.txt
     fi
     # analysis does not properly install data file yet
     # thus use local install
-    pip3 install -e .
+    pip_install_use_cache -e .
     cd ..
 done
